fb0e8cfaa44814af43512a671533052a76445e09
Cristian Camilo Londoño <cricalonos@gmail.com>
2018-04-27 20:44:21 -0500

    Versión inicial del proyecto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera) {
+  }
 
+  base64Image: string = "assets/imgs/img-perfil.jpg";
+  takePicture(): void {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+
+    });
   }
 
 }
